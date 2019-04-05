dbd800e9acc796bc2e9da160c399302865e72d2f
LiFu <753641657@qq.com>
2019-04-06 00:36:53 +0800

    restore hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 from flask import Flask
-from flask_script import Manager
-
 app = Flask(__name__)
-manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -13,4 +10,4 @@ def user(name):
   return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == "__main__":
-  manager.run()
+  app.run(debug=True)
