72ec93ed1f2d9b221d01464a226407159467f0e2
LiFu <753641657@qq.com>
2019-07-04 10:56:44 +0800

    add relationship in User and Role

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@
 # @Time    : 2019/6/28 2:37
 # @Author  : Li Fu
 
+import os
+
 from flask import Flask, render_template, session, redirect, url_for, flash
 
 from flask_bootstrap import Bootstrap
@@ -12,20 +14,48 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-from datetime import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 bootstrap = Bootstrap(app)
 
 moment = Moment(app)
+
+db = SQLAlchemy(app)
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/', methods=['GET', 'POST'])
