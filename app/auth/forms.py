93c1bfb05396af6f20e6726f802d669cc5ac0a25
LiFu-ChrisLee <753641657@qq.com>
2019-07-10 15:15:35 +0800

    add login and logout

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -11,16 +11,14 @@ from ..models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                               'Usernames must have only letters, '
--- app/models.py
+++ app/models.py
@@ -3,8 +3,10 @@
 # @Author  : Li Fu
 
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 from . import db
+from . import login_manager
 
 
 # db model
@@ -19,9 +21,10 @@ class Role(db.Model):
 
 
 # db model
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
@@ -39,3 +42,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+# use unique code to load user
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
