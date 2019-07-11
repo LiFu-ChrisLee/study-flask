4b62442fed27a90e98a087ebd3b3416eec7c8087
LiFu-ChrisLee <753641657@qq.com>
2019-07-11 15:29:41 +0800

    add feature of confirm email

--- app/email.py
+++ app/email.py
@@ -2,3 +2,25 @@
 # @Time    : 2019/7/8 11:20
 # @Author  : Li Fu
 
+
+from threading import Thread
+from flask import render_template, current_app
+from flask_mail import Message
+from . import mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+# send mail
+def send_email(to, subject, template, **kwargs):
+    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=current_app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[current_app, msg])
+    thr.start()
+    return thr
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@
 # @Time    : 2019/7/8 11:20
 # @Author  : Li Fu
 
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -28,6 +30,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -39,6 +42,23 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('ascii')
+
+    def confirm(self, token):
+        s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except Exception as e:
+            print(e)
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
